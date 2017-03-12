e843fb84504213d0248b42f7befba384efb305fa
x86dev <x86dev@users.noreply.github.com>
2017-03-12 22:57:20 +0100

    More customization.

--- source/50_file.sh
+++ source/50_file.sh
@@ -34,14 +34,3 @@ alias dsstore="find . -name '*.DS_Store' -type f -ls -delete"
 
 # Aliasing eachdir like this allows you to use aliases/functions as commands.
 alias eachdir=". eachdir"
-
-# Create a new directory and enter it
-function md() {
-  mkdir -p "$@" && cd "$@"
-}
-
-# Fast directory switching
-mkdir -p $DOTFILES/caches/z
-_Z_NO_PROMPT_COMMAND=1
-_Z_DATA=$DOTFILES/caches/z/z
-. $DOTFILES/vendor/z/z.sh
