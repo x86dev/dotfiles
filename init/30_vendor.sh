4fc43c650e541554afbcff2bff873de837dd05a2
x86dev <x86dev@users.noreply.github.com>
2017-03-13 22:36:06 +0100

    Install (link) dircolors-solarized and vim-colors-solarized.

---
+++ init/30_vendor.sh
@@ -0,0 +1,5 @@
+# Install dircolors.
+ln -sf ${DOTFILES}/vendor/dircolors-solarized ${HOME}/.dircolors
+
+# Link solarized color scheme for Vim.
+ln -sf ${DOTFILES}/vendor/vim-colors-solarized/ ${HOME}/.vim/bundle/vim-colors-solarized
