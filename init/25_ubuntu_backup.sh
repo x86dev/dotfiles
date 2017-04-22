5e66a695821b74a78f5566cd216b72b23a09fe83
x86dev <x86dev@users.noreply.github.com>
2017-04-22 23:05:48 +0200

    init/25_ubuntu_backup.sh: Fix for previous commit.

--- init/25_ubuntu_backup.sh
+++ init/25_ubuntu_backup.sh
@@ -1,6 +1,5 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 is_ubuntu || return 1
 
-#!/bin/sh
-# Install KeePass 2 + dependencies.
-apt-get install -y keepass2 mono mono-dmcs xsel
+# Install required dependencies.
+apt-get install -y duplicity rsync ca-cacert
