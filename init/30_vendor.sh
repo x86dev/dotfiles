790ba63a05faef461381b1c49df56b47ddd0bf9e
x86dev <x86dev@users.noreply.github.com>
2017-04-24 10:35:57 +0200

    Added fzf installation + sourcing stuff.

--- init/30_vendor.sh
+++ init/30_vendor.sh
@@ -3,3 +3,7 @@ ln -sf ${DOTFILES}/vendor/dircolors-solarized ${HOME}/.dircolors
 
 # Link solarized color scheme for Vim.
 ln -sf ${DOTFILES}/vendor/vim-colors-solarized/ ${HOME}/.vim/bundle/vim-colors-solarized
+
+# Install fzf.
+## @todo Build fzf from scratch (Go required) instead of downloading the binary?
+${DOTFILES}/vendor/fzf/install --bin --key-bindings --completion --no-update-rc
---
+++ source/50_fzf.sh
@@ -0,0 +1,14 @@
+MY_FZF=${DOTFILES}/vendor/fzf
+
+if [[ ! "$PATH" == *$MY_FZF/bin* ]]; then
+  export PATH="$PATH:$MY_FZF/bin"
+fi
+
+# Auto-completion.
+[[ $- == *i* ]] && source "$MY_FZF/shell/completion.bash" 2> /dev/null
+
+# Key bindings.
+source "$MY_FZF/shell/key-bindings.bash"
+
+# Customization
+export FZF_DEFAULT_OPTS='--height 40% --border'
