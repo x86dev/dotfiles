d17371489478115639f78b50f8f164d8eb6bb7de
x86dev <x86dev@users.noreply.github.com>
2017-06-18 00:28:55 +0200

    init: Added xdotool to 25_ubuntu_keepass.sh.

--- init/25_ubuntu_keepass.sh
+++ init/25_ubuntu_keepass.sh
@@ -3,4 +3,4 @@ is_ubuntu || return 1
 
 #!/bin/sh
 # Install KeePass 2 + dependencies.
-sudo apt-get install -y keepass2 mono mono-dmcs xsel
+sudo apt-get install -y keepass2 mono mono-dmcs xsel xdotool
