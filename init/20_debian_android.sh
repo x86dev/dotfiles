a5dfd10722447cd3edebc0faa8219d83437557d3
x86dev <x86dev@users.noreply.github.com>
2017-09-16 20:56:58 +0200

    init/20_debian_android.sh: Also install mtp-tools.

--- init/20_debian_android.sh
+++ init/20_debian_android.sh
@@ -6,4 +6,6 @@ e_header "udev Android rules"
 sudo install -m644 \
     "$DOTFILES/bin/etc/udev/rules.d/51-android.rules" "/etc/udev/rules.d/51-android.rules"
 
+sudo apt-get install -y mtp-tools
+
 sudo service udev restart
