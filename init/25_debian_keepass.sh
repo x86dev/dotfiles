b3d92ab93a4a84204fa233f2cfaf5a489bfff4f9
x86dev <x86dev@users.noreply.github.com>
2019-05-23 11:59:34 +0200

    init/25_debian_keepass: Added curl as a prerequisite, needed to download stuff.

--- init/25_debian_keepass.sh
+++ init/25_debian_keepass.sh
@@ -1,9 +1,9 @@
 # Debian-only stuff. Abort if not Debian.
-is_debian || return 1
+#is_debian || return 1
 
 #!/bin/sh
 # Install KeePass 2 + dependencies.
-sudo apt-get install -y keepass2 mono-dmcs xsel xdotool
+sudo apt-get install -y curl keepass2 mono-dmcs xsel xdotool
 
 # Install Yubico Personalization Tools.
 # This is required for the KeeChallenge plugin.
