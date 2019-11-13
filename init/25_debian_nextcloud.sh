20f54e94b800dcb5269e69520eb3785261d2b5b9
x86dev <x86dev@users.noreply.github.com>
2019-11-13 11:13:54 +0100

    init: Condensed software installation.

--- init/25_debian_nextcloud.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Add additional PPA repositories.
-sudo add-apt-repository ppa:nextcloud-devs/client
-sudo add-apt-repository ppa:pbek/qownnotes
-
-# Install NextCloud + QOwnNotes.
-sudo apt-get -qq update
-sudo apt-get install -y nextcloud-client qownnotes
--- init/25_debian_qownnotes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo add-apt-repository -y ppa:pbek/qownnotes
-sudo apt-get update && sudo apt-get install -y qownnotes
