b03f7dbafca4854255518d86e9321d9061935d38
x86dev <x86dev@users.noreply.github.com>
2020-05-04 13:15:16 +0200

    Make Zsh the default shell.

--- init/25_debian_common.sh
+++ init/25_debian_common.sh
@@ -6,9 +6,11 @@
 # Install Oh My Zsh.
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
 
+# Make Zsh default.
+chsh -s $(which zsh)
+
 # Git tweaking.
 # Security warning: Will store in *plaintext* on disk!
 $(cd "$DOTFILES"; \
   git config --global credential.helper store; \
   git config remote.origin.push HEAD )
-
