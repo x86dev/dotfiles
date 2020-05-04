318b44da74e1967648263106b02c4fa0de108e36
x86dev <x86dev@users.noreply.github.com>
2020-05-04 13:27:05 +0200

    Install restic.

--- init/25_debian_common.sh
+++ init/25_debian_common.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# TODO
-# Install restic, lazygit
+# Install restic.
+MY_RESTIC_VER=0.9.5
+MY_RESTIC_TMP=$(mktemp -d)
+sh -c "$(wget -O $MY_RESTIC_TMP/restic.bz2 https://github.com/restic/restic/releases/download/v${MY_RESTIC_VER}/restic_${MY_RESTIC_VER}_linux_amd64.bz2)"
+bunzip2 "$MY_RESTIC_TMP/restic.bz2"
+chmod +x "$MY_RESTIC_TMP/restic"
+sudo mv "$MY_RESTIC_TMP/restic" /usr/local/bin/
+rm -rf "$MY_RESTIC_TMP"
 
 # Install lazygit.
 sudo add-apt-repository ppa:lazygit-team/release
