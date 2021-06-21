d59a04cf6653ddecdd2d225735918c774108627d
x86dev <github@x86dev.com>
2021-06-21 20:11:11 +0200

    init/25_debian_desktop.sh: Removed Qownnotes (now using Joplin), install nautilus-nextcloud.

--- init/25_debian_desktop.sh
+++ init/25_debian_desktop.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 sudo add-apt-repository -y ppa:ubuntu-mozilla-security/ppa
-sudo add-apt-repository -y ppa:pbek/qownnotes
 sudo add-apt-repository -y ppa:nextcloud-devs/client
 sudo add-apt-repository -y ppa:yubico/stable
 sudo add-apt-repository -y ppa:sebastian-stenzel/cryptomator
@@ -9,7 +8,7 @@ sudo add-apt-repository -y ppa:phoerious/keepassxc
 
 sudo apt-get update
 
-sudo apt-get install -y keepassxc meld nautilus-compare nextcloud-client qownnotes thunderbird
+sudo apt-get install -y keepassxc meld nautilus-compare nautilus-nextcloud thunderbird
 
 #
 # Upgrade lists:
