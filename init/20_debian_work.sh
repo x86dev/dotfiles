2c5051c71a2020b6bf497b015c706230412251fc
x86dev <github@x86dev.com>
2022-04-29 11:42:50 +0200

    Docs.

--- init/20_debian_work.sh
+++ init/20_debian_work.sh
@@ -4,6 +4,9 @@ sudo apt-get install -y --no-install-recommends yasm git subversion meld
 
 # Install GEF (GDB Enhanced Features -- https://github.com/hugsy/gef). Requires Python 3.
 wget -O ~/.gdbinit-gef.py -q https://gef.blah.cat/py
+# Capstone Engine: http://www.capstone-engine.org/
+# Keystone Engine: https://www.keystone-engine.org/
+# Unicorn:         https://pypi.org/project/unicorn/
 pip install capstone keystone unicorn
 
 # Install KDE devel script for GDB. Includes some nice Qt printing stuff ("qs", ++).
