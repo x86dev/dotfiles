690843a216e774c7fef566893c1eb087fa3fc773
x86dev <github@x86dev.com>
2022-04-29 11:17:20 +0200

    Added more GDB scripts (GEF + KDE Devel).

--- init/20_debian_work.sh
+++ init/20_debian_work.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-sudo apt-get install yasm subversion
-code --install-extension ms-vscode.cpptools-extension-pack jebbs.plantuml dotjoshjohnson.xml
+sudo apt-get install -y --no-install-recommends yasm git subversion meld
+
+# Install GEF (GDB Enhanced Features -- https://github.com/hugsy/gef). Requires Python 3.
+wget -O ~/.gdbinit-gef.py -q https://gef.blah.cat/py
+
+# Install KDE devel script for GDB. Includes some nice Qt printing stuff ("qs", ++).
+wget -O ~/.gdbinit-kde-devel-gdb -q https://raw.githubusercontent.com/KDE/kde-dev-scripts/master/kde-devel-gdb
