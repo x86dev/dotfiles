93b9cb7e27a83814fb2fe96ad03b99105417b1ba
x86dev <github@x86dev.com>
2022-04-29 14:43:19 +0200

    Added GDB GEP (for fzf / fuzzy finding).

--- init/20_debian_work.sh
+++ init/20_debian_work.sh
@@ -12,5 +12,10 @@ pip install capstone keystone unicorn
 # Install KDE devel script for GDB. Includes some nice Qt printing stuff ("qs", ++).
 wget -O ~/.gdbinit-kde-devel-gdb -q https://raw.githubusercontent.com/KDE/kde-dev-scripts/master/kde-devel-gdb
 
-# Install STL GDB evaluators/views/utilities
+# Install STL GDB evaluators/views/utilities.
 wget -O ~/.gdbinit-stl -q https://raw.githubusercontent.com/fzzjoy/gdbinit/master/.gdbinit
+
+# Install GEP (for fzf / fuzzy finding).
+git clone https://github.com/lebr0nli/GEP.git ~/GEP && \
+    cd ~/GEP && \
+    sh install.sh
--- link/.gdbinit
+++ link/.gdbinit
@@ -7,3 +7,6 @@ source ~/.gdbinit-stl
 
 # Use GEF (GDB Enhanced Features) -- https://github.com/hugsy/gef
 source ~/.gdbinit-gef.py
+
+# Use GEP (for fzf / fuzzy finder).
+source ~/GEP/gdbinit-gep
