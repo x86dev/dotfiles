d6da1e137242efc26256ccd58a0e9d0af8bef3f5
x86dev <github@x86dev.com>
2022-06-30 15:29:40 +0200

    Added more stuff for 25_debian_desktop.sh.

--- init/25_debian_desktop.sh
+++ init/25_debian_desktop.sh
@@ -5,10 +5,9 @@ sudo add-apt-repository -y ppa:nextcloud-devs/client
 sudo add-apt-repository -y ppa:yubico/stable
 sudo add-apt-repository -y ppa:sebastian-stenzel/cryptomator
 sudo add-apt-repository -y ppa:phoerious/keepassxc
-
 sudo apt-get update
 
-sudo apt-get install -y keepassxc meld nautilus-nextcloud thunderbird
+sudo apt-get install -y firefox thunderbird keepassxc meld nextcloud-desktop
 
 # Clipboard utilities for X / Wayland. Needed for tmux, for instance.
 sudo apt-get install -y xsel wl-clipboard
