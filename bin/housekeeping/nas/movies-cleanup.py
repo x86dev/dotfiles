473ff8cf30f3e7ba2abc1811622a425f26341f60
x86dev <github@x86dev.com>
2023-03-29 13:18:08 +0200

    movies-cleanup: Removed unnecessary semicolons.

--- bin/housekeeping/nas/movies-cleanup.py
+++ bin/housekeeping/nas/movies-cleanup.py
@@ -17,200 +17,200 @@ import re # Regular expressions.
 import shutil # For rmtree().
 import sys
 
-g_fDryRun        = True;
-g_cDupesTotal    = 0;
-g_cbDupesTotal   = 0;
-g_cbDupesRemoved = 0;
-g_sLogFile       = '';
-g_fRecursive     = False;
-g_cVerbosity     = 0;
+g_fDryRun        = True
+g_cDupesTotal    = 0
+g_cbDupesTotal   = 0
+g_cbDupesRemoved = 0
+g_sLogFile       = ''
+g_fRecursive     = False
+g_cVerbosity     = 0
 
-tFileDupe = namedtuple('tFileDupe', 'ext, prio');
+tFileDupe = namedtuple('tFileDupe', 'ext, prio')
 
 g_arrVideoTypes = [ tFileDupe('mkv' , 0),
                    tFileDupe('avi' , 10),
                    tFileDupe('mp4' , 20),
                    tFileDupe('divx', 30),
-                   tFileDupe('wmv' , 50) ];
+                   tFileDupe('wmv' , 50) ]
 
 # Minimum size a video file must have (in bytes).
 # This is useful for not treating a video file as the newest (=best) file
 # if this file is just e.g. a sample / demo file.
 #
 # Set to 0 to disable this check.
-g_cbVideoSizeMin = 700 * (1024 * 1024); ## @todo Use a constant for MB as bytes?
+g_cbVideoSizeMin = 700 * (1024 * 1024) ## @todo Use a constant for MB as bytes?
 
-g_arrDirsToDelete = [ '.*/_UNPACK_*' ];
+g_arrDirsToDelete = [ '.*/_UNPACK_*' ]
 
-g_arrFileExtsToDelete = [ 'url', 'nzb', 'exe', 'com', 'bat', 'cmd', 'sample', 'scr', 'rar', 'zip', '7z' ];
+g_arrFileExtsToDelete = [ 'url', 'nzb', 'exe', 'com', 'bat', 'cmd', 'sample', 'scr', 'rar', 'zip', '7z' ]
 
 # Taken from: http://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
 # Slightly modified to handle byte sizes as well.
 def fileSizeToStr(size):
     if size:
-        size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
-        i = int(math.floor(math.log(size,1024)));
-        p = math.pow(1024,i);
-        s = round(size/p,2);
+        size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+        i = int(math.floor(math.log(size,1024)))
+        p = math.pow(1024,i)
+        s = round(size/p,2)
         if s > 0:
-            return '%s %s' % (s,size_name[i]);
-    return '0B';
+            return '%s %s' % (s,size_name[i])
+    return '0B'
 
 def getModTime(sPath):
-    mTime = os.path.getmtime(sPath);
-    return datetime.datetime.fromtimestamp(mTime);
+    mTime = os.path.getmtime(sPath)
+    return datetime.datetime.fromtimestamp(mTime)
 
 def deleteFile(sFile):
-    global g_fDryRun;
-    print("\tDeleting file: %s" % sFile);
+    global g_fDryRun
+    print("\tDeleting file: %s" % sFile)
     if g_fDryRun:
-        return;
+        return
     try:
-        os.remove(sFile);
+        os.remove(sFile)
     except OSError as e:
-        print("\tError deleting file \"%s\": %s" % (sFile, str(e)));
+        print("\tError deleting file \"%s\": %s" % (sFile, str(e)))
 
 def deleteDir(sDir, fRecursive):
-    global g_fDryRun;
-    print("\tDeleting directory: %s" % sDir);
+    global g_fDryRun
+    print("\tDeleting directory: %s" % sDir)
     if g_fDryRun:
-        return;
+        return
     try:
         if fRecursive:
-            shutil.rmtree(sDir, ignore_errors = False);
+            shutil.rmtree(sDir, ignore_errors = False)
         else:
-            os.rmdir(sDir);
+            os.rmdir(sDir)
     except OSError as e:
-        print("\tError deleting directory \"%s\": %s" % (sDir, str(e)));
+        print("\tError deleting directory \"%s\": %s" % (sDir, str(e)))
 
 def fileIsMultipart(sDir, sFile):
-    _ = sDir;
-    _ = sFile;
+    _ = sDir
+    _ = sFile
 
 def cleanupDupes(sDir, fRecursive):
-    global g_cDupesTotal;
-    global g_cbDupesTotal;
+    global g_cDupesTotal
+    global g_cbDupesTotal
     for sCurDir, aSubDirs, aFiles in os.walk(sDir):
         for sDir in aSubDirs:
-            cleanupDupes(sDir, fRecursive);
-        mtimeDir = os.path.getmtime(sCurDir);
-        arrDupes = [];
+            cleanupDupes(sDir, fRecursive)
+        mtimeDir = os.path.getmtime(sCurDir)
+        arrDupes = []
         for sFile in aFiles:
-            sFileAbs = os.path.join(sCurDir, sFile);
-            sName, sExt = os.path.splitext(sFileAbs);
-            sName = sName.lower();
+            sFileAbs = os.path.join(sCurDir, sFile)
+            sName, sExt = os.path.splitext(sFileAbs)
+            sName = sName.lower()
             if len(sExt) > 1: # Skip the dot (.)
-                sExt = sExt[1:];
+                sExt = sExt[1:]
             for curType in g_arrVideoTypes:
                 if curType.ext == sExt:
-                    arrDupes.append(sFileAbs);
+                    arrDupes.append(sFileAbs)
             for curExt in g_arrFileExtsToDelete:
                 if curExt == sExt:
-                    print("File \"%s\" is junk" % sFileAbs);
-                    deleteFile(sFileAbs);
+                    print("File \"%s\" is junk" % sFileAbs)
+                    deleteFile(sFileAbs)
 
         if len(arrDupes) >= 2:
-            print("Directory \"%s\" contains %d entries:" % (sCurDir, len(arrDupes)));
-            tsFileNewest = datetime.datetime(1970, 1, 1);
-            sFileNewest  = '';
+            print("Directory \"%s\" contains %d entries:" % (sCurDir, len(arrDupes)))
+            tsFileNewest = datetime.datetime(1970, 1, 1)
+            sFileNewest  = ''
             for curDupe in arrDupes:
-                tsFileLastMod = getModTime(curDupe);
-                cbFileSize    = os.path.getsize(curDupe);
-                print("\t%s (last modified %s, %s)" % (curDupe, tsFileLastMod, fileSizeToStr(cbFileSize)));
+                tsFileLastMod = getModTime(curDupe)
+                cbFileSize    = os.path.getsize(curDupe)
+                print("\t%s (last modified %s, %s)" % (curDupe, tsFileLastMod, fileSizeToStr(cbFileSize)))
                 if tsFileLastMod >  tsFileNewest \
                 and cbFileSize   >= g_cbVideoSizeMin:
-                    sFileNewest  = curDupe;
-                    tsFileNewest = tsFileLastMod;
+                    sFileNewest  = curDupe
+                    tsFileNewest = tsFileLastMod
             if sFileNewest:
-                print("\tNewest file: %s" % sFileNewest);
+                print("\tNewest file: %s" % sFileNewest)
                 for curDupe in arrDupes:
                     if      curDupe != sFileNewest \
                     and not fileIsMultipart(sCurDir, curDupe):
-                        g_cDupesTotal  += 1;
-                        g_cbDupesTotal += os.path.getsize(curDupe);
-                        deleteFile(curDupe);
+                        g_cDupesTotal  += 1
+                        g_cbDupesTotal += os.path.getsize(curDupe)
+                        deleteFile(curDupe)
             else:
-                print("\tWarning: Unable to determine newest file!");
+                print("\tWarning: Unable to determine newest file!")
 
         # Delete unwanted junk.
         for sRegEx in g_arrDirsToDelete:
             if   re.compile(sRegEx).match(sCurDir) \
             and  sCurDir \
             and  sCurDir != "/":
-                print("Directory \"%s\" is junk" % sCurDir);
-                deleteDir(sCurDir, True);
+                print("Directory \"%s\" is junk" % sCurDir)
+                deleteDir(sCurDir, True)
 
         # Delete empty directories.
         if os.path.isdir(sCurDir) and len(os.listdir(sCurDir)) == 0:
-            print("Directory \"%s\" is empty" % sCurDir);
-            deleteDir(sCurDir, False);
-            sCurDir = None;
+            print("Directory \"%s\" is empty" % sCurDir)
+            deleteDir(sCurDir, False)
+            sCurDir = None
 
         # Re-apply directory modification time.
         if sCurDir:
-            os.utime(sCurDir, (-1, mtimeDir));
+            os.utime(sCurDir, (-1, mtimeDir))
 
         if not fRecursive:
-            break;
+            break
 
 def printHelp():
-    print("--delete");
-    print("    Deletion mode: Files/directories *are* modified and/or deleted.");
-    print("--help or -h");
-    print("    Prints this help text.");
-    print("--recursive or -R");
-    print("    Also processes sub directories.");
-    print("-v");
-    print("    Increases logging verbosity. Can be specified multiple times.");
-    print("\n");
+    print("--delete")
+    print("    Deletion mode: Files/directories *are* modified and/or deleted.")
+    print("--help or -h")
+    print("    Prints this help text.")
+    print("--recursive or -R")
+    print("    Also processes sub directories.")
+    print("-v")
+    print("    Increases logging verbosity. Can be specified multiple times.")
+    print("\n")
 
 def main():
-    global g_cDupesTotal;
-    global g_cbDupesTotal;
-    global g_fDryRun;
-    global g_fRecursive;
-    global g_cVerbosity;
+    global g_cDupesTotal
+    global g_cbDupesTotal
+    global g_fDryRun
+    global g_fRecursive
+    global g_cVerbosity
 
     if len(sys.argv) <= 1:
-        print("Must specify a path!");
-        sys.exit(2);
+        print("Must specify a path!")
+        sys.exit(2)
 
     try:
-        aOpts, aArgs = getopt.gnu_getopt(sys.argv[1:], "hRv", ["delete", "help", "recursive" ]);
+        aOpts, aArgs = getopt.gnu_getopt(sys.argv[1:], "hRv", ["delete", "help", "recursive" ])
     except getopt.error as msg:
-        print(msg);
+        print(msg)
         print("For help use --help")
-        sys.exit(2);
+        sys.exit(2)
 
     for o in aOpts:
         if o == "--delete":
-            g_fDryRun = False;
+            g_fDryRun = False
         if o in ("-h", "--help"):
-            printHelp();
-            sys.exit(0);
+            printHelp()
+            sys.exit(0)
         if o in ("-R", "--recursive"):
-            g_fRecursive = True;
+            g_fRecursive = True
         if o in ("-v", "--verbose"):
-            g_cVerbosity += 1;
+            g_cVerbosity += 1
 
     if len(aArgs) <= 0:
-        print("No source directory specified!");
-        sys.exit(2);
+        print("No source directory specified!")
+        sys.exit(2)
 
     today = datetime.datetime.today()
     print("Started: %02d/%02d/%02d %02d:%02d:%02d" % (today.year, today.month, today.day, today.hour, today.minute, today.second))
 
     if g_fDryRun:
-        print("*** Dryrun mode -- no files/directories deleted! ***");
+        print("*** Dryrun mode -- no files/directories deleted! ***")
 
     for sDir in aArgs:
-        print("Processing: %s" % sDir);
-        cleanupDupes(sDir, g_fRecursive);
+        print("Processing: %s" % sDir)
+        cleanupDupes(sDir, g_fRecursive)
 
-    print("Total dupes: %ld (%s)" % (g_cDupesTotal, fileSizeToStr(g_cbDupesTotal)));
+    print("Total dupes: %ld (%s)" % (g_cDupesTotal, fileSizeToStr(g_cbDupesTotal)))
 
     if g_fDryRun:
-        print("*** Dryrun mode -- no files/directories deleted! ***");
+        print("*** Dryrun mode -- no files/directories deleted! ***")
 
 if __name__ == "__main__":
-    main();
+    main()
