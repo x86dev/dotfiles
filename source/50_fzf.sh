889da2ff9da0cc5c9fdeada27cd3531443fae094
x86dev <github@x86dev.com>
2024-01-31 15:23:49 +0100

    source/50_fzf.sh: Got rid of key bindings stuff; will be used by fzf install script.

--- source/50_fzf.sh
+++ source/50_fzf.sh
@@ -1,14 +1,2 @@
-FZF_BASE=${HOME}/opt/fzf
-
-if [[ ! "$PATH" == *$FZF_BASE* ]]; then
-  export PATH="$PATH:$FZF_BASE"
-fi
-
-# Auto-completion.
-#[[ $- == *i* ]] && source "$FZF_BASE/shell/completion.$MY_SHELL" 2> /dev/null
-
-# Key bindings.
-source "$FZF_BASE/shell/key-bindings.$MY_SHELL"
-
 # Customization
 export FZF_DEFAULT_OPTS='--height 40% --border'
